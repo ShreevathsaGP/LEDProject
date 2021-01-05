9db666a4984d96acc2fbb3d925dfa6b9167a2c59
ShreevathsaGP <gpshreevathsa@gmail.com>
2021-01-05 21:03:51 +0530

    organize examples

--- SGPLed/SGPLed.h
+++ SGPLed/SGPLed.h
@@ -9,7 +9,7 @@
 #include <string>
 #include <unordered_map>
 
-/* #include <Arduino.h> */
+#include <Arduino.h>
 
 //------ INCLUDES ----------------------------------------------------------------------------------------------------
 
