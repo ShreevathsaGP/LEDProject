6ec48954062e9f57d60df1663aac7aed836b9fc1
ShreevathsaGP <gpshreevathsa@gmail.com>
2021-01-08 18:37:11 +0530

    deleted

---
+++ SGPLed/SGPLed.h
@@ -0,0 +1,78 @@
+#ifndef SGPLed_H
+#define SGPLed_H
+
+//------ INCLUDES ----------------------------------------------------------------------------------------------------
+
+#include <Arduino.h>
+#include <Queue.h>
+#include <string.h>
+#include <CharMap.h>
+
+
+//------ INCLUDES ----------------------------------------------------------------------------------------------------
+
+//------ GLOBALS -----------------------------------------------------------------------------------------------------
+
+// matrix specs 
+const int no_rows = 8; // SHOULD NOT BE MORE THAN 10 [Writing Lib Will Fail]
+const int no_columns = 28;
+
+// colours
+enum Colours {
+    Black, Red, Green, Blue, Yellow, Orange, Purple, Pink
+};
+
+// processes
+enum Processes {
+    Writing, Tetris, Pong, Watch, Nothing
+};
+
+// led-matrix state
+struct State {
+    Colours board[no_rows][no_columns];
+    Processes current_process;
+    Queue<String> instructions;
+    String current_instruction;
+    int instruction_index;
+};
+
+//------ GLOBALS -----------------------------------------------------------------------------------------------------
+
+//------ WRITING -----------------------------------------------------------------------------------------------------
+
+#define NO_SPACES 2
+class Writer {
+    public:
+        // constructor
+        Writer(State &in_state);
+
+        // write animation
+        void write(String in_string);
+
+        // space (between letters)
+        void add_space(int no, String &in_string);
+
+
+    private:
+        // initialize letter-mappings
+        bool init_mappings();
+        
+        // variables
+        State &current_state;
+        String sample_string;
+        int letter_width;
+        int temp;
+        int middle;
+        int no_flat;
+
+        int top;
+        int bottom;
+        int constraint;
+        
+        // letter-mappings (column wise operations)
+        CharMap letter_map;
+};
+
+//------ WRITING -----------------------------------------------------------------------------------------------------
+
+#endif // SGPLed_H
---
+++ SGPLed/examples/hello_world/one_letter.ino
@@ -0,0 +1,99 @@
+#include <FastLED.h>
+
+#define DELAY 100
+#define LED_PIN 7
+#define LED_BRAND WS2812B
+
+// CONSTANTS
+const int no_rows = 8;
+const int no_columns = 28;
+
+// ENUMS
+enum Orientation {
+  Left, Right
+};
+enum Colours {
+    Black, Red
+};
+
+// GLOBALS
+CRGB strip[(no_columns * no_rows)];
+int fast_index = 0;
+Colours local[no_rows][no_columns];
+Orientation orientation;
+Colour temp;
+unsigned long current_time = 0;
+unsigned long previous_time = 0;
+
+// shift board
+void shift(Colours matrix[no_rows][no_columns]) {
+  for (int i = 0; i < no_rows; i++) {
+    for (int j = 1; j < no_columns; j++) {
+      temp = matrix[i][j];
+      matrix[i][j - 1] = temp;
+    }
+  }
+
+  for (int j = 1; j < no_rows; j++) {
+    matrix[j][no_columns - 1] = Black;
+  }
+}
+
+// draw the matrix [in actual LED]
+void draw() {
+  orientation = Left;
+  for (int row = 0; row < no_rows; row++) {
+    if (orientation == Left) {
+      // left <-- right
+      for (int col = no_columns - 1; col > -1; col--) {
+        if (local[row][col] == Colours::Red) {
+          strip[fast_index] = CRGB::Red;
+        } else {
+            strip[fast_index] = CRGB::Black;
+        }
+        fast_index += 1;
+        orientation = Right;
+      }
+    } else {
+      // left --> right
+      for (int col = 0; col < no_columns; col++) {
+        if (local[row][col] == Colours::Red) {
+          strip[fast_index] = CRGB::Red;
+        } else {
+            strip[fast_index] = CRGB::Black;
+        }
+        fast_index += 1;
+        orientation = Left;
+      }
+    }
+  }
+  FastLED.show();
+  fast_index = 0;
+}
+
+void setup() {
+    FastLED.addLeds<LED_BRAND, LED_PIN, GRB>(strip, (no_columns * no_rows));
+    FastLED.setBrightness(50);
+
+    for (int i = 2; i < no_rows - 2; i++) {
+        local[i][no_columns - 2] = Colours::Red;
+    }
+
+    for (int i = no_columns - 6; i < no_columns - 2; i++) {
+        local[1][i] = Colours::Red;
+        local[no_rows - 2][i] = Colours::Red;
+    }
+
+    for (int i = 1; i < no_rows - 1; i++) {
+        local[i][no_columns - 7] = Colours::Red;
+    }
+}
+
+void loop() {
+    current_time = millis();
+    if (current_time - previous_time > DELAY) {
+        previous_time = current_time;
+        draw();
+        shift(local);
+    }
+}
