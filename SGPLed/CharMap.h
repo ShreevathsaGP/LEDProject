05bfe7950b7947cfbe06e35532cd62eecd8c5d15
ShreevathsaGP <gpshreevathsa@gmail.com>
2021-01-08 16:47:42 +0530

    char map size reduction

--- SGPLed/CharMap.h
+++ SGPLed/CharMap.h
@@ -1,14 +1,14 @@
 #ifndef CharMap_H
 #define CharMap_H
 
-#define NO_ASCII 128
+#define NO_ASCII 75
 #include <string.h>
 
 class CharMap {
     public:
         // overloading [] returns string (reference)
         String& operator [](char in_char) {
-            return array[(int)in_char];
+            return array[((int)in_char - 48)];
         }
         
     private:
