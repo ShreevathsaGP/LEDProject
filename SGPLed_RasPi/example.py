e7ececb83d5c7b5ffc321435afbc7294ece3b220
ShreevathsaGP <gpshreevathsa@gmail.com>
2021-01-28 14:52:12 +0530

    working version

--- SGPLed_RasPi/example.py
+++ SGPLed_RasPi/example.py
@@ -1,7 +1,7 @@
 # Raspberry Pi Example
 
 # raspberry pi imports
-from rpi_ws281x import *
+# from rpi_ws281x import *
 
 # regular imports
 from collections import defaultdict
@@ -19,8 +19,12 @@ current_time = 0
 previous_time = time.time()
 int_string = ""
 running = True
-orientation = 0 # 0 = left | 1 = right
-strip_index = 0
+orientation = 0 # 0 = right | 1 = left
+actual_index = 0
+
+# initialize strip
+# strip = Adafruit_Neopixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# strip.begin()
 
 def shift():
     for row in range(NO_ROWS):
@@ -30,27 +34,38 @@ def shift():
     for row in range(0, NO_ROWS):
         current_state.board[row][NO_COLUMNS - 1] = Colours.Black
 
+def console_show():
+    for row in range(NO_ROWS):
+        print("[ ", end = "")
+        for col in range(NO_COLUMNS):
+            print(current_state.board[row][col].value, end = " ")
+
+        print("]")
+    print('')
+
 def show():
     actual_index = 0
     orientation = 0 # left
     for row in range(NO_ROWS):
-        if orientation == 0:
+        if orientation == 1:
             # left <-- right
             for col in range(NO_COLUMNS - 1, -1, -1):
-                if current_state.board[row][col] == Colours.Red:    
-                    # light up led
-                    print(row, col)
+                # print(RedChannel[current_state.board[row][col]], GreenChannel[current_state.board[row][col]], BlueChannel[current_state.board[row][col]])
+                # strip.setPixelColor(actual_index, Color(RedChannel[current_state.board[row][col]], GreenChannel[current_state.board[row][col]], BlueChannel[current_state.board[row][col]]))
+                continue
+
                 actual_index += 1
-                orientation = 1 # right
+            orientation = 0 # right
         else:
             # left --> right
             for col in range(0, NO_COLUMNS, 1):
-                if current_state.board[row][col] == Colours.Red:
-                    # light up led
-                    print(row, col)
+                # print(RedChannel[current_state.board[row][col]], GreenChannel[current_state.board[row][col]], BlueChannel[current_state.board[row][col]])
+                # strip.setPixelColor(actual_index, Color(RedChannel[current_state.board[row][col]], GreenChannel[current_state.board[row][col]], BlueChannel[current_state.board[row][col]]))
+                continue
+
                 actual_index += 1
-                orientation = 0 # left
-    strip.show()
+            orientation = 1 # left
+    # strip.show()
 
 def update():
     if current_state.current_process == Processes.Writing:
@@ -70,7 +85,6 @@ def update():
             if int_string == ' ':
                 break
 
-            # light up led
             if int_string == None:
                 current_state.instruction_index = len(current_state.current_instruction)
                 break
@@ -78,6 +92,7 @@ def update():
             current_state.board[int(int_string)][NO_COLUMNS - 1] = Colours.Red
 
 def render():
+    console_show()
     show()
 
     if current_state.current_process == Processes.Writing:
--- SGPLed_RasPi/glob.py
+++ SGPLed_RasPi/glob.py
@@ -11,17 +11,17 @@ WRITING_TEST = 'abcdefghijklmnopqrstuvwxyz!"#$%&'
 WRITING_TEST += "' ()*+,-./:;<=>?@[\]^_`{|}~123456789"
 
 # time specs
-DELAY = 0.1
+DELAY = 0.01
 
 # matrix specs
 NO_ROWS = 8 
-NO_COLUMNS = 110
+NO_COLUMNS = 28
 NO_LEDS = NO_ROWS * NO_COLUMNS
 
 # led configs
 LED_COUNT      = 16      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+# LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
@@ -30,14 +30,46 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # colours
 class Colours(Enum):
-    Black = 0
-    Red = 1
+    Black = 0 
+    Red = 1 
     Green = 2
     Blue = 3
     Yellow = 4
     Orange = 5
     Purple = 6
-    Pink = 7
+
+# Red Channel Values
+RedChannel = {
+        Colours.Black: 0,
+        Colours.Red : 255,
+        Colours.Green : 0,
+        Colours.Blue: 0,
+        Colours.Yellow: 255,
+        Colours.Orange: 255,
+        Colours.Purple: 128
+        }
+
+# Green Channel Values
+GreenChannel = {
+        Colours.Black: 0,
+        Colours.Red : 0,
+        Colours.Green : 255,
+        Colours.Blue: 0,
+        Colours.Yellow: 255,
+        Colours.Orange: 165,
+        Colours.Purple: 0
+        }
+
+# Blue Channel Values
+BlueChannel = {
+        Colours.Black: 0,
+        Colours.Red : 0,
+        Colours.Green : 0,
+        Colours.Blue: 255,
+        Colours.Yellow: 0,
+        Colours.Orange: 0,
+        Colours.Purple:128 
+        }
 
 # processes
 class Processes(Enum):
